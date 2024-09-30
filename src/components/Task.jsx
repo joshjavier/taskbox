1acd22b37d05ddcf7cb51fea60b007fc698b3ba2
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-01 06:50:05 +0800

    change task background to red

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -33,6 +33,7 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
+          style={{ backgroundColor: 'red' }}
         />
       </label>
 
